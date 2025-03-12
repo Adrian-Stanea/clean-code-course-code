e225d3630e74c436f887d094b719352e1d796fed
Adrian-Stanea <Adrian.Stanea@analog.com>
2025-03-12 20:27:16 +0200

    Add challenge solution
    Signed-off-by: Adrian-Stanea <Adrian.Stanea@analog.com>

--- 01-challenge-problem.py
+++ 01-challenge-problem.py
@@ -1,35 +1,56 @@
-class Point:
-    def __init__(self, coordX, coordY):
-        self.coordX = coordX
-        self.coordY = coordY
+from pydantic.dataclasses import dataclass
+
+
+@dataclass
+class Point2D:
+    X: float = 0.0
+    Y: float = 0.0
 
 
 class Rectangle:
-    def __init__(self, starting_point, broad, high):
-        self.starting_point = starting_point
-        self.broad = broad
-        self.high = high
+    def __init__(self, origin: Point2D, width: float, height: float):
+        self.origin = origin
+        self.width = width
+        self.height = height
 
-    def area(self):
-        return self.broad * self.high
+    def get_area(self):
+        return self.width * self.height
 
-    def end_points(self):
-        top_right = self.starting_point.coordX + self.broad
-        bottom_left = self.starting_point.coordY + self.high
-        print('Starting Point (X)): ' + str(self.starting_point.coordX))
-        print('Starting Point (Y)): ' + str(self.starting_point.coordY))
-        print('End Point X-Axis (Top Right): ' + str(top_right))
-        print('End Point Y-Axis (Bottom Left): ' + str(bottom_left))
+    def print_end_points(self):
+        """
+        Prints the coordinates of the rectangle's end points.
+        """
+        bottom_left, bottom_right, top_right = self.compute_edges()
 
 
-def build_stuff():
-    main_point = Point(50, 100)
-    rect = Rectangle(main_point, 90, 10)
+        print(f"Origin: {self.origin}")
+        print(f"Top Right: {top_right}")
+        print(f"Bottom Left: {bottom_left}")
+        print(f"Bottom Right: {bottom_right}")
 
-    return rect
+    def compute_edges(self):
+        offsetX = self.origin.X + self.width
+        offsetY = self.origin.Y + self.height
+
+        bottom_left = Point2D(self.origin.X, offsetY)
+        bottom_right = Point2D(offsetX, offsetY)
+        top_right = Point2D(offsetX, self.origin.Y)
+        return bottom_left, bottom_right, top_right
+
+    def display(self):
+        print(f"Origin: {self.origin}")
+        print(f"Width: {self.width}")
+        print(f"Height: {self.height}")
 
 
-my_rect = build_stuff()
+def build_rectangle() -> Rectangle:
+    point = Point2D(X=50, Y=100)
+    rectangle = Rectangle(origin=point, width=90, height=10)
+    return rectangle
 
-print(my_rect.area())
-my_rect.end_points()
+
+rectangle = build_rectangle()
+rectangle.display()
+
+print(f"Rectangle Area: {rectangle.get_area()}")
+rectangle.print_end_points()
